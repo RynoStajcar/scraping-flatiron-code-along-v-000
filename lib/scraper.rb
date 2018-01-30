b87ced820e6ba6068d64083f1e636d577da3a9ef
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-30 17:16:55 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
   def get_courses
     html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
     doc = Nokogiri::HTML(html)
-    doc.css(".post").text
+    doc.css(".post")
   end
 
   def make_courses
